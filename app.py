39eea5271a71298db46686fa6caf71a5a6464728
Alexei Korol <korolalexei@gmail.com>
2023-12-09 01:01:47 +0400

    Add session management for generated images

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
 import requests
-import json
 
 app = Flask(__name__)
+app.secret_key = '6599109069'  # It's essential to keep this key secret in production
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     error_message = None
-    generated_images = []
-    form_data = {}
+    form_data = request.form.to_dict()
+
     cost_per_image = {
         'Standard_1024x1024': 0.040,
         'Standard_1024x1792': 0.080,
@@ -19,42 +19,43 @@ def index():
     }
 
     if request.method == 'POST':
-        form_data = request.form
         api_key = form_data.get('api_key')
         prompt = form_data.get('prompt')
         size = form_data.get('size')
         n = int(form_data.get('number_of_images', '1'))
-        quality = 'HD' if form_data.get('quality') == 'on' else 'Standard'
-
+        quality = 'HD' if form_data.get('quality') else 'Standard'
         headers = {
             'Authorization': f'Bearer {api_key}',
             'Content-Type': 'application/json'
         }
-
         data = {
             'model': 'dall-e-3',
             'prompt': prompt,
             'n': n,
             'size': size
         }
-
         try:
             response = requests.post('https://api.openai.com/v1/images/generations', headers=headers, json=data)
             response.raise_for_status()
             result = response.json()
-
+            generated_images = []
             for item in result['data']:
                 generated_images.append({
                     'url': item['url'],
                     'cost': cost_per_image[f'{quality}_{size}'],
-                    'prompt': prompt
+                    'prompt': prompt  # Include the prompt used for the image
                 })
+            # Save generated images to session to persist history
+            if 'generated_images' not in session:
+                session['generated_images'] = []
+            session['generated_images'].extend(generated_images)
+            session.modified = True
 
         except requests.exceptions.HTTPError as err:
             error_message = str(err)
 
-    return render_template('index.html', error_message=error_message, generated_images=generated_images, form_data=form_data)
-
+    # Use images from session for history
+    return render_template('index.html', error_message=error_message, generated_images=session.get('generated_images', []), form_data=form_data)
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ readme.md
@@ -0,0 +1,39 @@
+# Dall-e3 Artwork Generator (DAWG)
+
+DAWG is a Flask web application that interfaces with OpenAI's DALL-E 3 API to generate images based on user input.
+
+## Features
+
+- Image generation with customizable prompts
+- Persistence of image generation history
+- Modern and responsive user interface
+
+## Installation
+
+Clone the repository and navigate into the directory:
+```bash
+git clone https://your-repository-url
+cd your-repository-directory
+```
+
+Install required dependencies:
+```bash
+pip install -r requirements.txt
+```
+
+## Usage
+
+Run the Flask application:
+```bash
+flask run
+```
+
+Open a web browser and navigate to `http://127.0.0.1:5000/` to access the application.
+
+## Contributing
+
+Contributions are welcome! Please fork the repository and submit a pull request with your changes.
+
+## License
+
+This project is licensed under the MIT License - see the LICENSE file for details.
